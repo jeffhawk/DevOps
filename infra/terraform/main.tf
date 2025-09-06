f6ee23e78407bb884d7dd1a4e6e9311e3193ba32
Jefferson Eduardo <46302791+jeffhawk@users.noreply.github.com>
2025-09-06 02:27:59 -0300

    corrigindo arquivos tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -70,8 +70,8 @@ resource "aws_ecs_service" "app" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = var.subnet_ids
-    security_groups = var.security_group_ids
+    subnets          = var.subnet_ids
+    security_groups  = var.security_group_ids
     assign_public_ip = true
   }
 }
